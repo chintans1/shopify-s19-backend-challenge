8f8bef968e27e8380972bd548ff309a81da14403
Chintan Shah <chintans98@gmail.com>
2019-01-19 23:29:36 -0500

    Add service and repository layer for Cart
    - tests are included as well
    - cart can be created with products, products can be added on top and then you can complete the purchase where we check inventory

--- src/main/java/com/chintan/shopifychallenge/repository/ProductRepository.java
+++ src/main/java/com/chintan/shopifychallenge/repository/ProductRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 /*
  * An instance of this will contain methods to query the table "product" by using methods such as
- * findAll() and findById() and it also allows you add and delete new products through methods like save() and delete().
+ * findAll() and findById() and it also allows you add and delete products through methods like save() and delete().
  */
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {}
