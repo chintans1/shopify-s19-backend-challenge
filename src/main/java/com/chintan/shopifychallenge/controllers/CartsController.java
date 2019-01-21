aca177f407901d6d32fa4b130c2140cf3a51974c
Chintan Shah <chintans98@gmail.com>
2019-01-20 19:25:27 -0500

    Add Swagger docs for all the endpoints

--- src/main/java/com/chintan/shopifychallenge/controllers/CartsController.java
+++ src/main/java/com/chintan/shopifychallenge/controllers/CartsController.java
@@ -5,6 +5,7 @@ import com.chintan.shopifychallenge.models.Product;
 import com.chintan.shopifychallenge.models.controllers.CreateCart;
 import com.chintan.shopifychallenge.services.CartService;
 import com.chintan.shopifychallenge.services.ProductService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/carts", produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(description = "Endpoints for creating new carts, adding products to a cart and completing purchases.")
 public class CartsController {
   public static final String CART_DOES_NOT_EXIST_ERROR_MESSAGE = "This cart does not exist in the database.";
 
@@ -29,6 +31,10 @@ public class CartsController {
   }
 
   @GetMapping("{cartId}")
+  @ApiOperation("Returns the cart details that includes a list of products and totalCost for the given cart. " +
+                "An exception is thrown if the cart is not found in the database.")
+  @ApiImplicitParam(name = "cartId", value = "The ID of the cart that you want details of", dataType = "int",
+                    required = true, paramType = "path")
   public Cart viewCart(@PathVariable final Integer cartId) {
     Optional<Cart> singleCart = cartService.viewCart(cartId);
 
@@ -37,8 +43,12 @@ public class CartsController {
   }
 
   @PostMapping
-  public Cart createCart(@RequestBody(required = false) CreateCart createCart) {
-    if (Objects.isNull(createCart)) return cartService.createNewCart();
+  @ApiOperation("Returns a newly created cart. If the list of productIDs were passed in, the cart returned will reflect that.")
+  public Cart createCart(@ApiParam(value = "OPTIONAL: You may create a new cart with a list of products. " +
+                                           "The list should contain product IDs that you want in the cart.")
+                         @RequestBody(required = false) CreateCart createCart) {
+    if (Objects.isNull(createCart) || Objects.isNull(createCart.getProductIds()))
+      return cartService.createNewCart();
 
     List<Product> productsInCart = createCart.getProductIds()
         .stream()
@@ -51,6 +61,14 @@ public class CartsController {
   }
 
   @PutMapping("{cartId}/products/{productId}")
+  @ApiOperation("Returns the updated cart details where the product you specified to be added is present and the " +
+                "total cost reflects those changes. An exception is thrown if the product is already present in the cart.")
+  @ApiImplicitParams({
+      @ApiImplicitParam(name = "cartId", value = "The ID of the cart that you want to add a product to",
+                        dataType = "int", required = true, paramType = "path"),
+      @ApiImplicitParam(name = "productId", value = "The ID of the product you want to add to the cart",
+                        dataType = "int", required = true, paramType = "path")
+  })
   public Cart addProductToCart(@PathVariable final Integer cartId, @PathVariable final Integer productId) {
     final Optional<Product> productToAdd = productService.getProductById(productId);
     if (!productToAdd.isPresent()) throw new IllegalArgumentException(ProductsController.PRODUCT_DOES_NOT_EXIST_ERROR_MESSAGE);
@@ -59,6 +77,11 @@ public class CartsController {
   }
 
   @PutMapping("{cartId}/complete")
+  @ApiOperation("This will complete the purchase of the products that are in the cart. The product inventory will " +
+                "be updated and the cart will get subsequently deleted. An exception is thrown if the cart is not " +
+                "present in the database or one of the products is out of stock.")
+  @ApiImplicitParam(name = "cartId", value = "The ID of the cart that you complete the purchase for",
+                    dataType = "int", required = true, paramType = "path")
   public void completePurchase(@PathVariable final Integer cartId) {
     cartService.completeCartPurchase(cartId);
   }
--- src/main/java/com/chintan/shopifychallenge/controllers/ProductsController.java
+++ src/main/java/com/chintan/shopifychallenge/controllers/ProductsController.java
@@ -4,6 +4,9 @@ import com.chintan.shopifychallenge.models.Product;
 import com.chintan.shopifychallenge.services.ProductService;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +16,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products", produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(description = "Endpoints for fetching all products, fetching in-stock products or fetching specific products using title or ID.")
 public class ProductsController {
   public static final String EMPTY_PRODUCT_TITLE_ERROR_MESSAGE = "You cannot pass in an empty product title.";
   public static final String PRODUCT_DOES_NOT_EXIST_ERROR_MESSAGE = "This product does not exist in the database.";
@@ -25,23 +29,35 @@ public class ProductsController {
   }
 
   @GetMapping
+  @ApiOperation("Returns all the products that are currently present in the marketplace.")
   public List<Product> getAllProducts() {
     return productService.getAllProducts();
   }
 
   @GetMapping(params = "showInStockOnly")
+  @ApiOperation("Returns all in-stock products that are currently present in the marketplace if showInStockOnly is true. " +
+                "Otherwise, all the products are returned.")
+  @ApiImplicitParam(name = "showInStockOnly", value = "Boolean to denote whether you want to fetch only in-stock products or not.",
+                    dataType = "boolean", required = true, paramType = "query")
   public List<Product> getInStockProducts(@RequestParam(required = false, defaultValue = "false") final boolean showInStockOnly) {
     if (showInStockOnly) return productService.getAllInStockProducts();
     else return getAllProducts();
   }
 
   @GetMapping(params = "productTitle")
+  @ApiOperation("Returns all products present in the marketplace that match the product title you specify.")
+  @ApiImplicitParam(name = "productTitle", value = "The title of product that you want to see details for.",
+                    dataType = "string", required = true, paramType = "query")
   public List<Product> getProductsByTitle(@RequestParam(required = false) final String productTitle) {
     Preconditions.checkArgument(!Strings.isNullOrEmpty(productTitle), EMPTY_PRODUCT_TITLE_ERROR_MESSAGE);
     return productService.getProductsByTitle(productTitle);
   }
 
   @GetMapping("{productId}")
+  @ApiOperation("Return details for a single product using the given product ID. " +
+                "An exception is thrown if the product is not present in the marketplace.")
+  @ApiImplicitParam(name = "productId", value = "The ID of the product that you wants details of", dataType = "int",
+                    required = true, paramType = "path")
   public Product getSingleProduct(@PathVariable final Integer productId) {
     Optional<Product> singleProduct = productService.getProductById(productId);
 
